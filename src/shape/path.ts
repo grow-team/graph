49262289ccc9e0f3b1732665c972a1b133063d9f
anjia <anjia@360.cn>
2017-09-01 23:39:20 +0800

    更改jialj因为熬夜犯二了解了真相之后似乎还在不能原谅地叹气而导致的问题

--- src/shape/path.ts
+++ src/shape/path.ts
@@ -7,22 +7,11 @@ export class Path extends Shape {
 
     constructor(ctx, x,y, ...args) {
         super(ctx, x,y);
-
-        this.args = args;
-        this.ctx.beginPath();
-
-        this.ctx.moveTo(x,y);
-        for(let i=0; i<args.length-1; i+=2){
-            this.ctx.lineTo(args[i], args[i+1]);
-        }
-
-        this.ctx.closePath();
-        this.ctx.stroke();
+        this.draw(x, y, ...args);
         console.log('...constructor() Path');
     }
 
     draw(x, y , ...args) {
-
         super.start();
 
         this.ctx.moveTo(x,y);
@@ -31,18 +20,7 @@ export class Path extends Shape {
         }
 
         super.end();
-
         console.log('...draw() Path');
     }
 
 }
-
-// rollup存在编译问题 ，当连续两个方式都使用了 ...args，在第一个方法中将args转换成了数组，此时传递给第二个方法时候，参数个数出现问题
-
-// let _draw =  function(ctx,x,y,...args){
-//     console.log( args instanceof Array);
-//     ctx.moveTo(x,y);
-//     for(let i=0; i<args.length-1; i+=2){
-//         ctx.lineTo(args[i], args[i+1]);
-//     }
-// }
